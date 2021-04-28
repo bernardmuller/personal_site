eabffd0baac903693c4078f1f41e4d16f1a429b3
Bernard Muller <b.mullerjnr@gmail.com>
2021-04-28 13:14:34 +0200

    Added target_blank to a tags

--- src/Components/About.js
+++ src/Components/About.js
@@ -14,11 +14,11 @@ function About() {
                         while still use the core principles of planning and design that I learned in architecture school.</p>
                     <p>In my free time, you can find me in front of my computer busy fixing broken code I have written. I love learning new things, 
                         and since I found my interest in programming I have been fully immersed in it and I'm learning new things everyday. 
-                        Im always on platforms like <a href="https://www.youtube.com/">Youtube</a>, <a href="https://www.coursera.org/">Coursera</a>, 
-                        <a href="https://www.freecodecamp.org/">Freecodecamp</a>, or <a href="https://www.udemy.com/">Udemy</a> busy learning new things programming related, 
+                        Im always on platforms like <a href="https://www.youtube.com/" target="_blank">Youtube</a>, <a href="https://www.coursera.org/" target="_blank">Coursera</a>, 
+                        <a href="https://www.freecodecamp.org/" target="_blank">Freecodecamp</a>, or <a href="https://www.udemy.com/" target="_blank">Udemy</a> busy learning new things programming related, 
                         reading through documentation of APIs I might use in future projects, or scrolling through the 
-                        <a href="https://www.reddit.com/r/webdev/">r/webdev</a> subreddit on Reddit to see what other people are building. 
-                        I like listening to podcasts like <a href="https://www.codenewbie.org/podcast">CodeNewbie</a>, 
+                        <a href="https://www.reddit.com/r/webdev/" target="_blank">r/webdev</a> subreddit on Reddit to see what other people are building. 
+                        I like listening to podcasts like <a href="https://www.codenewbie.org/podcast" target="_blank">CodeNewbie</a>, 
                         a podcast dedicated to helping new developers on their transition into the industry.</p>
                     <p>I'm currently working hard on my goal to build projects and land a job in tech.</p>
                 </div>                
